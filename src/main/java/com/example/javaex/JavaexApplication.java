d6d0059b09dcc560b9f3d416a717cba138d5123a
Mariano Ramirez <Mariano.X.Ramirez.-ND@disney.com>
2018-07-24 23:54:19 -0300

    First commit

---
+++ src/main/java/com/example/javaex/JavaexApplication.java
@@ -0,0 +1,12 @@
+package com.example.javaex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaexApplication.class, args);
+    }
+}
